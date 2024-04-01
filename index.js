43cdb352e9452af81030b83196138f5507f3e76a
AlishaKK <162887724+AlishaKK@users.noreply.github.com>
2024-04-02 00:37:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please guess a number between (1-6) :",
+    },
+]);
+if (answer.userGuessNumber === randomNumber) {
+    console.log("Correct! you won the game ");
+}
+else {
+    console.log("Wrong! You lost the game");
+}
